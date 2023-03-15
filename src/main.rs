b04a3a711df73518618e6abd17bdec9e0bf900e1
Grokepeer <matteo.minardi2014@gmail.com>
2023-03-15 01:17:55 +0100

    10000 testlen

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use std::{
     str,
-    io::prelude::*,
+    ops::Rem,
+    io::{prelude::*, BufReader},
     net::TcpStream
 };
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:2088").unwrap();
-    let mut read = [0; 128];
-    stream.read(&mut read);
-    println!("> {}", str::from_utf8(&read).unwrap());
+    let mut buffer = BufReader::new(stream.try_clone().unwrap());
+    // let mut read = String::from("");
+    // buffer.read(&mut read);
+    // println!("> {:?}", str::from_utf8(&read).unwrap());
 
-    for i in 1..200 {
-        let cmd = format!("set {} in _basedb to yepthisisitdef", i);
-        println!("{cmd}");
+    let testlen = 100000;
+    print!("<");
+
+    for i in 1..testlen {
+        let cmd = format!("set {} in _basedb to yepthisisitdef\n", i);
         stream.write(cmd.as_bytes());
         let mut read = [0; 128];
         stream.read(&mut read);
-        println!("> {}", str::from_utf8(&read).unwrap());
+        
+        if i % 1000 == 0 {
+            println!("{} / {:?}", i, str::from_utf8(&read).unwrap_or("-").trim_matches(char::from(0)));
+        }
     }
 }
