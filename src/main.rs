44ba120989b3fb8da150725b1e0175e1daea6959
Grokepeer <matteo.minardi@sheartail.net>
2023-03-24 14:12:27 +0100

    updated test parameters

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ fn main() {
     // buffer.read(&mut read);
     // println!("> {:?}", str::from_utf8(&read).unwrap());
 
-    let testlen = 100000;
+    let testlen = 20000;
     print!("<");
 
     for i in 1..testlen {
-        let cmd = format!("set {} in _basedb to yepthisisitdef\n", i);
+        let cmd = format!("get {} from _basedb\n", i);
         stream.write(cmd.as_bytes());
         let mut read = [0; 128];
         stream.read(&mut read);
         
-        if i % 1000 == 0 {
-            println!("{} / {:?}", i, str::from_utf8(&read).unwrap_or("-").trim_matches(char::from(0)));
+        if i % 500 == 0 {
+            println!("{}\t / {:?}", i, str::from_utf8(&read).unwrap_or("-").trim_matches(char::from(0)));
         }
     }
 }
